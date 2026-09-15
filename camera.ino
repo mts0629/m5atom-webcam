ecadee900717a6c589cca15c9f857349a6ba2cc9
mts0629 <soratobi93@gmail.com>
2026-09-15 23:59:22 +0900

    Connect to WiFi AP

--- camera.ino
+++ camera.ino
@@ -1,12 +1,29 @@
 #include <M5Unified.h>
+#include <WiFi.h>
+
+// AP info
+const char *ssid = "*";
+const char *pass = "*";
 
 void setup() {
+  auto cfg = M5.config();
+  M5.begin(cfg);
+
   Serial.begin(115200);
   delay(500);
+
+  WiFi.begin(ssid, pass);
+  
+  Serial.println("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+  Serial.printf("Connected: SSID=%s\n", ssid);
+
+  Serial.printf("IP=");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  Serial.println("hello");
-
   delay(1000);
 }
