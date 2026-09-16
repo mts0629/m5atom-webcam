77ad4b2c08da583f67f276952e0863f61fe0d415
mts0629 <soratobi93@gmail.com>
2026-09-16 23:59:58 +0900

    Test HTTP access

--- camera.ino
+++ camera.ino
@@ -1,5 +1,6 @@
 #include <M5Unified.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 // AP info
 const char *ssid = "*";
@@ -20,10 +21,25 @@ void setup() {
   }
   Serial.printf("Connected: SSID=%s\n", ssid);
 
-  Serial.printf("IP=");
+  Serial.print("IP=");
   Serial.println(WiFi.localIP());
 }
 
+const char *uri = "https://example.com/";
+
 void loop() {
-  delay(1000);
+  M5.update();
+
+  // Test access
+  HTTPClient http;
+  http.begin(uri);
+  int http_code = http.GET();
+  if (http_code == HTTP_CODE_OK) {
+    auto payload = http.getString();
+    Serial.println(payload);
+  } else {
+    Serial.println("HTTP GET failed");
+  }
+
+  delay(10000);
 }
